afa51f3d39661284b21a9b76666f5018f777be01
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2020-07-13 17:23:57 +0100

    change path to input

--- NF-ChIP_alignment/test/test_NF-ChIP_alignment.sh
+++ NF-ChIP_alignment/test/test_NF-ChIP_alignment.sh
@@ -11,7 +11,7 @@ ml Graphviz
 ## UPDATE PIPLINE
 nextflow pull nf-core/chipseq
 
-## RUN alignment
+# RUN alignment
 nextflow run nf-core/chipseq \
 -r 1.2.0 \
 -c /camp/home/thierya/scratch/otic-reprogramming/NF-ChIP_alignment/conf/crick.config \
@@ -21,14 +21,25 @@ nextflow run nf-core/chipseq \
 --save_reference \
 -resume
 
+
+# nextflow run nf-core/chipseq \
+# -r 1.2.0 \
+# -c /camp/home/thierya/scratch/otic-reprogramming/NF-ChIP_alignment/conf/crick.config \
+# --input /camp/home/thierya/scratch/otic-reprogramming/NF-ChIP_alignment/test/ChIP_design_test.csv \
+# --fasta /camp/home/thierya/working/genomes/galgal6/Gallus_gallus.GRCg6a.dna.toplevel.fa \
+# --gtf /camp/home/thierya/working/genomes/galgal6/Gallus_gallus.GRCg6a.97.gtf \
+# --bwa_index /camp/home/thierya/scratch/otic-reprogramming/NF-ChIP_alignment/test/results/genome/BWAIndex/Gallus_gallus.GRCg6a.dna.toplevel.fa \
+# -resume
+
+
 # nextflow run nf-core/chipseq \
 # -r 1.2.0 \
 # -profile docker \
 # --input /Users/alex/dev/repos/otic-reprogramming/NF-ChIP_alignment/test/ChIP_design_test.csv \
 # --fasta /Users/alex/dev/genomes/galgal5/Gallus_gallus.Gallus_gallus-5.0.dna.toplevel.fa \
 # --gtf /Users/alex/dev/genomes/galgal5/Gallus_gallus.Gallus_gallus-5.0.94_modified.gtf \
-# -c /Users/alex/dev/repos/otic-reprogramming/NF-ChIP_alignment/test/local.config \
-# --save_reference \
+# --bwa_index /Users/alex/dev/repos/otic-reprogramming/NF-ChIP_alignment/genome/BWAIndex/Gallus_gallus.GRCg6a.dna.toplevel.fa \
+# -c /Users/alex/dev/repos/otic-reprogramming/NF-ChIP_alignment/conf/local.config \
 # -resume
 
 
