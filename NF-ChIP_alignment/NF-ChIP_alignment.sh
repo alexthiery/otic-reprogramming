98c4f98c6e3fe44bb8724855d436770e8238b85e
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2020-07-15 11:14:56 +0100

    edit run chipseq alignment with singularity on camp

--- NF-ChIP_alignment/NF-ChIP_alignment.sh
+++ NF-ChIP_alignment/NF-ChIP_alignment.sh
@@ -9,8 +9,7 @@ export TERM=xterm
 ## LOAD REQUIRED MODULES
 ml purge
 ml Nextflow/20.01.0
-ml Singularity/3.4.2
-ml Graphviz
+ml Singularity/2.6.0-foss-2016b
 
 ## UPDATE PIPLINE
 nextflow pull nf-core/chipseq
@@ -18,7 +17,8 @@ nextflow pull nf-core/chipseq
 # RUN alignment
 nextflow run nf-core/chipseq \
 -r 1.2.0 \
--c /camp/home/thierya/scratch/otic-reprogramming/NF-ChIP_alignment/conf/crick.config \
+-profile crick \
+-with-singularity /camp/apps/misc/stp/babs/nf-core/singularity/chipseq/1.2.0/nfcore-chipseq-1.2.0.img \
 --input /camp/home/thierya/scratch/otic-reprogramming/NF-ChIP_alignment/ChIP_design.csv \
 --fasta /camp/home/thierya/working/genomes/galgal6/Gallus_gallus.GRCg6a.dna.toplevel.fa \
 --gtf /camp/home/thierya/working/genomes/galgal6/Gallus_gallus.GRCg6a.97.gtf \
