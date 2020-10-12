a31143d0dc0ef5776cd46ce97b8a00ef5a900ff4
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2020-10-12 11:48:24 +0100

    add sox8 and lmx1a alignments to pipeline

---
+++ NF-lmx1a_alignment/NF-lmx1a_alignment.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+#SBATCH --job-name=NF-sox8
+#SBATCH -t 72:00:00
+#SBATCH --mail-type=ALL,ARRAY_TASKS
+#SBATCH --mail-user=alex.thiery@crick.ac.uk
+
+export TERM=xterm
+
+## LOAD REQUIRED MODULES
+ml purge
+ml Nextflow/20.01.0
+ml Singularity/2.6.0-foss-2016b
+ml Graphviz
+
+## UPDATE PIPLINE
+nextflow pull nf-core/rnaseq
+
+## RUN alignment
+nextflow run nf-core/rnaseq \
+  -r 1.4.2 \
+  -profile crick \
+  -with-singularity /camp/apps/misc/stp/babs/nf-core/singularity/rnaseq/1.4.2/nfcore-rnaseq-1.4.2.img \
+  --reads "/camp/home/thierya/working/raw_data/rnaoe/*_{1,2}.fastq.gz" \
+  --fasta "/camp/home/thierya/working/genomes/galgal6/Gallus_gallus.GRCg6a.dna.toplevel.fa" \
+  --gtf "/camp/home/thierya/working/genomes/galgal6/Gallus_gallus.GRCg6a.97.gtf" \
+  --outdir results-sox8_oe \
+  --email alex.thiery@crick.ac.uk
