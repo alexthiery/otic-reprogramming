7536c8c185576f792a43c55ac2996255fef60720
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2020-10-30 14:44:48 +0000

    change the atac and chip alignment scripts to include bedgraph

--- NF-ChIP_alignment/NF-ChIP_alignment.sh
+++ NF-ChIP_alignment/NF-ChIP_alignment.sh
@@ -23,6 +23,7 @@ nextflow run nf-core/chipseq \
 --fasta /camp/home/thierya/working/genomes/galgal6/Gallus_gallus.GRCg6a.dna.toplevel.fa \
 --gtf /camp/home/thierya/working/genomes/galgal6/Gallus_gallus.GRCg6a.97.gtf \
 --macs_gsize 1.05e9 \
+--save_macs_pileup \
 --skip_diff_analysis \
 --outdir results-chip \
 --email alex.thiery@crick.ac.uk \
