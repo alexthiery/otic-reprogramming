b79d0b90a23168073e45af322e6f7ae74e4bbc81
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2020-11-13 20:56:09 +0000

    change paths for functional enrichment analysis

--- bin/functional_enrichment_analysis.R
+++ bin/functional_enrichment_analysis.R
@@ -1,13 +1,13 @@
 #!/usr/bin/env Rscript
 
-output_path = './output'
-dir.create(output_path)
+output_path = './output/'
+dir.create(output_path, recursive = T)
 
 library(gprofiler2)
 library(dplyr)
 library(ggplot2)
 
-putative_enhancers <- read.delim(list.files('./', pattern = '.txt', full.names = TRUE))
+putative_enhancers <- read.delim(list.files(pattern = '*txt', full.names = TRUE))
 
 # run functional enrichment analysis using GO:biological process and KEGG terms
 fea_res <- gost(putative_enhancers$Entrez.ID, organism = 'ggallus', sources = c('GO:BP', 'KEGG'))
@@ -16,7 +16,7 @@ fea_res <- gost(putative_enhancers$Entrez.ID, organism = 'ggallus', sources = c(
 # gost(putative_enhancers$Entrez.ID, organism = 'ggallus', sources = c('GO:BP', 'KEGG'), as_short_link = TRUE)
 
 # select enriched terms of interest and generate bar plot
-plot_data <- output$result %>%
+plot_data <- fea_res$result %>%
   filter(term_id %in% c("GO:0022008", "GO:0007399", "GO:0030182", "GO:0007411", "GO:0060070", "KEGG:04330", "KEGG:04310")) %>%
   select(c(p_value, term_name, term_id)) %>%
   mutate(-log10(p_value)) %>%
@@ -24,7 +24,7 @@ plot_data <- output$result %>%
   mutate(term_name = paste0(term_name, ' (', term_id, ")")) %>%
   mutate(term_name = factor(term_name, levels = term_name))
 
-png(paste0(output_path, "functional_enrichment.png"), height = 20, width = 25, units = "cm", res = 400)
+png(paste0(output_path, "functional_enrichment.png"), height = 12, width = 13, units = "cm", res = 400)
 ggplot(plot_data, aes(x = term_name, y = -log10(p_value), label = term_name)) +
   geom_bar(stat='identity', width=0.5, fill='steelblue') +
   coord_flip() +
@@ -34,5 +34,6 @@ ggplot(plot_data, aes(x = term_name, y = -log10(p_value), label = term_name)) +
         axis.text.y=element_blank(),
         axis.ticks.y=element_blank(),
         axis.line.y=element_blank()) +
-  scale_y_continuous(expand = c(0, 0), limits = c(0, 10))
+  scale_y_continuous(expand = c(0, 0), limits = c(0, 10)) +
+  theme(plot.margin=unit(c(0.5,0.5,0.5,0.5),"cm"))
 graphics.off()
