68d050704cb5bc85fc11486329a7d93e2e3f21a2
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2020-11-18 11:43:38 +0000

    update sox8 rnaseq alignment to nextflow dsl2 pipeline

--- NF-sox8_alignment/NF-sox8_alignment.sh
+++ NF-sox8_alignment/NF-sox8_alignment.sh
@@ -8,8 +8,8 @@ export TERM=xterm
 
 ## LOAD REQUIRED MODULES
 ml purge
-ml Nextflow/20.01.0
-ml Singularity/2.6.0-foss-2016b
+ml Nextflow/20.07.1
+ml Singularity/3.4.2
 ml Graphviz
 
 ## UPDATE PIPLINE
@@ -17,10 +17,9 @@ nextflow pull nf-core/rnaseq
 
 ## RUN alignment
 nextflow run nf-core/rnaseq \
-  -r 1.4.2 \
+  -r 2.0 \
   -profile crick \
-  -with-singularity /camp/apps/misc/stp/babs/nf-core/singularity/rnaseq/1.4.2/nfcore-rnaseq-1.4.2.img \
-  --reads "/camp/home/thierya/working/raw_data/Sox8_OE/190815_K00181_0165_AHCK3WBBXY/*_{1,2}.fastq.gz" \
+  --input crick_samplesheet.csv \
   --fasta "/camp/home/thierya/working/genomes/galgal6/Gallus_gallus.GRCg6a.dna.toplevel.fa" \
   --gtf "/camp/home/thierya/working/genomes/galgal6/Gallus_gallus.GRCg6a.97.gtf" \
   --outdir results-sox8_oe \
