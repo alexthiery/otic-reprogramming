0e1166bb388e23d183694d4347999c0b99b88cd9
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2020-11-18 13:41:42 +0000

    move lmx1a alignment script out of subdir

--- NF-lmx1a_alignment.sh
+++ NF-lmx1a_alignment.sh
@@ -5,12 +5,15 @@
 #SBATCH --mail-user=alex.thiery@crick.ac.uk
 
 export TERM=xterm
-
 ## LOAD REQUIRED MODULES
 ml purge
-ml Nextflow/20.01.0
-ml Singularity/2.6.0-foss-2016b
+ml Nextflow/20.07.1
+ml Singularity/3.4.2
 ml Graphviz
+
+export TERM=xterm
+export NXF_VER=20.07.1
+export NXF_SINGULARITY_CACHEDIR=/camp/home/thierya/working/NF_singularity
 
 ## UPDATE PIPLINE
 nextflow pull nf-core/rnaseq
@@ -19,9 +22,8 @@ nextflow pull nf-core/rnaseq
 nextflow run nf-core/rnaseq \
   -r 1.4.2 \
   -profile crick \
-  -with-singularity /camp/apps/misc/stp/babs/nf-core/singularity/rnaseq/1.4.2/nfcore-rnaseq-1.4.2.img \
   --reads "/camp/home/thierya/working/raw_data/rnaoe/*_{1,2}.fastq.gz" \
-  --fasta "/camp/home/thierya/working/genomes/galgal6/Gallus_gallus.GRCg6a.dna.toplevel.fa" \
-  --gtf "/camp/home/thierya/working/genomes/galgal6/Gallus_gallus.GRCg6a.97.gtf" \
-  --outdir results-lmx1a \
+  # --fasta "/camp/home/thierya/working/genomes/galgal6/Gallus_gallus.GRCg6a.dna.toplevel.fa" \
+  # --gtf "/camp/home/thierya/working/genomes/galgal6/Gallus_gallus.GRCg6a.97.gtf" \
+  --outdir output/results-lmx1a \
   --email alex.thiery@crick.ac.uk
