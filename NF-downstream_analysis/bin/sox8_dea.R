219636bb22d88e4ff06c30961e93729e55214eab
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2020-11-19 09:24:30 +0000

    update sox8 dea pipeline to take new input data from dsl2 alignment

--- NF-downstream_analysis/bin/sox8_dea.R
+++ NF-downstream_analysis/bin/sox8_dea.R
@@ -24,14 +24,14 @@ if(length(commandArgs(trailingOnly = TRUE)) == 0){
 # Set paths and load data
 {
   if (opt$runtype == "user"){
-    output_path = "./output/sox8_oe/output/"
-    input_files <- list.files("./output/results-sox8_oe/featureCounts", pattern = "*.txt$", full.names = T)
+    output_path = "./output/NF-downstream_analysis/sox8_dea/output/"
+    input_file <- "./output/NF-sox8_alignment/featurecounts.merged.counts.tsv"
     
   } else if (opt$runtype == "nextflow"){
     cat('pipeline running through nextflow\n')
     
     output_path = "output/"
-    input_files <- list.files("featureCounts/", pattern = "*.txt$", full.names = T)
+    input_file <- "./featurecounts.merged.counts.tsv"
   }
   
   dir.create(output_path, recursive = T)
@@ -50,22 +50,9 @@ if(length(commandArgs(trailingOnly = TRUE)) == 0){
   library(openxlsx)
 }
 
-# samples WTCHG_706842_265195 and WTCHG_706842_266183 were not imported for downstream analysis due to low number of reads (see multiqc output)
-
-# make dictionary of sample names and readcount files
-# samples are renamed from original sample sheet for downstream analysis. Original sample IDs can be found under Sample_ReadMe.txt
-samples_IDs <- data.frame(Sample = c("Sox8OE1", "Sox8OE2", "Sox8OE3", "Control1", "Control2", "Control3"),
-                          ID = c("WTCHG_706842_201108", "WTCHG_706842_202120", "WTCHG_706842_203132", "WTCHG_706842_205156",
-                                 "WTCHG_706842_207180", "WTCHG_706842_208192"),
-                          stringsAsFactors = F)
-
 # read in count data and rename columns
-read_counts <- as.data.frame(read.table(input_files, header = T, stringsAsFactors = F))
-colnames(read_counts) <- unlist(lapply(colnames(read_counts), function(x) ifelse(grepl("WTCHG", x), samples_IDs$Sample[grepl(gsub("_1Aligned.*", "", x ), samples_IDs$ID)], x)))
+read_counts <- read.delim(input_file, stringsAsFactors = FALSE)
 colnames(read_counts)[1] <- "gene_id"
-
-# remove samples with poor quality
-read_counts <- read_counts[,!is.na(colnames(read_counts))]
 
 # if gene name exists then take gene name, else take ensembl ID and make new name column
 read_counts <- read_counts %>% mutate(gene_name = ifelse(!is.na(gene_name), gene_name, gene_id))
@@ -83,22 +70,17 @@ write.csv(read_counts, paste0(output_path, "read_counts.csv"), row.names = F)
 rownames(read_counts) <- read_counts$gene_id
 read_counts[,1:2] <- NULL
 
-
-
-
-
-
 ### Add sample group to metadata
-col_data <- as.data.frame(sapply(colnames(read_counts), function(x){ifelse(grepl("Sox8", x), "Sox8", "Control")}))
+col_data <- as.data.frame(sapply(colnames(read_counts), function(x){ifelse(grepl("sox8_oe", x), "sox8_oe", "control")}))
 colnames(col_data) <- "Group"
 
 ### Make deseq object and make Control group the reference level
 deseq <- DESeqDataSetFromMatrix(read_counts, design = ~ Group, colData = col_data)
 deseq$Group <- droplevels(deseq$Group)
-deseq$Group <- relevel(deseq$Group, ref = "Control")
+deseq$Group <- relevel(deseq$Group, ref = "control")
 
 # set plot colours
-plot_colours <- list(Group = c(Sox8 = "#a1d76a", Control = "#e9a3c9"))
+plot_colours <- list(Group = c(sox8_oe = "#a1d76a", control = "#e9a3c9"))
 
 ### Filter genes which have fewer than 10 readcounts
 deseq <- deseq[rowSums(counts(deseq)) >= 10, ]
@@ -114,7 +96,7 @@ graphics.off()
 # LFC shrinkage uses information from all genes to generate more accurate estimates. Specifically, the distribution of
 # LFC estimates for all genes is used (as a prior) to shrink the LFC estimates of genes with little information or high
 # dispersion toward more likely (lower) LFC estimates.
-res <- lfcShrink(deseq, coef="Group_Sox8_vs_Control", type="apeglm")
+res <- lfcShrink(deseq, coef="Group_sox8_oe_vs_control", type="apeglm")
 
 res$gene_name <- gene_annotations$gene_name[match(rownames(res), gene_annotations$gene_id)]
 
@@ -200,7 +182,7 @@ res_down <- res_down[order(res_down$log2FoldChange),]
 
 nrow(res_up)
 nrow(res_down)
-# 732 genes DE with padj 0.05 & abs(logFC) > 1.5 (561 upregulated, 171 downregulated)
+# 511 genes DE with padj 0.05 & abs(logFC) > 1.5 (399 upregulated, 112 downregulated)
 
 # Write DE data as a csv
 res_de <- rbind(res_up, res_down) %>% arrange(-log2FoldChange)
@@ -269,7 +251,7 @@ res_sub <- res[which(res$padj < 0.05 & abs(res$log2FoldChange) > 1.5), ]
 res_sub <- res_sub[order(-res_sub$log2FoldChange),]
 
 # plot heatmap of DE genes
-png(paste0(output_path, "Sox8OE_hm.png"), height = 30, width = 21, units = "cm", res = 200)
+png(paste0(output_path, "sox8_oe_hm.png"), height = 30, width = 21, units = "cm", res = 200)
 pheatmap(assay(rld)[rownames(res_sub),], cluster_rows=T, show_rownames=FALSE,
          show_colnames = F, cluster_cols=T, annotation_col=as.data.frame(colData(deseq)["Group"]),
          annotation_colors = plot_colours, scale = "row", treeheight_row = 20, treeheight_col = 25)
@@ -324,119 +306,6 @@ plot <- pheatmap(rld.plot[res_sub_TF$gene_name,], cluster_rows=T, show_rownames=
 cat(rownames(rld.plot[res_sub_TF$gene_name,][plot$tree_row[["order"]],]), file=paste0(output_path, "sox8_DE_TFs.txt"))
 
 # plot DE TFs
-png(paste0(output_path, "Sox8OE_TFs_hm.png"), height = 25, width = 25, units = "cm", res = 200)
+png(paste0(output_path, "sox8_oe_TFs_hm.png"), height = 25, width = 25, units = "cm", res = 200)
 plot
 graphics.off()
-
-
-
-# ###########
-
-# # comparing with NC RNAseq
-
-# Load data from Williams et al. 2019 Developmental Cell
-williams_data <- tempfile()
-download.file("https://ndownloader.figshare.com/files/12750314", williams_data)
-load(williams_data)
-
-
-#########
-# Get biomart GO annotations for TFs
-#########
-
-ensembl = useMart("ensembl",dataset="ggallus_gene_ensembl")
-TF_subset <- getBM(attributes=c("ensembl_gene_id", "go_id", "name_1006", "namespace_1003"),
-                   filters = 'ensembl_gene_id',
-                   values = de_5to6$EnsemblID,
-                   mart = ensembl)
-
-# subset genes based on transcription factor GO terms
-TF_subset <- TF_subset$ensembl_gene_id[TF_subset$go_id %in% c('GO:0003700', 'GO:0043565', 'GO:0000981')]
-
-
-
-
-# Filter transcription factors from NC 5-6ss which are abs(log2FC > 1.5) & padj < 0.05 in citrine+ cells vs citrine- cells
-de_5to6 <- de_5to6[abs(de_5to6$log2FoldChange) > 1.5 &
-                     !is.na(de_5to6$log2FoldChange) &
-                     de_5to6$padj < 0.05 &
-                     !is.na(de_5to6$padj) &
-                     de_5to6$EnsemblID %in% TF_subset,]
-
-# Filter transcription factors from NC 8-10ss which are abs(log2FC > 1.5) & padj < 0.05 in citrine+ cells vs citrine- cells
-de_8to10 <- de_8to10[abs(de_8to10$log2FoldChange) > 1.5 &
-                       !is.na(de_8to10$log2FoldChange) &
-                       de_8to10$padj < 0.05 &
-                       !is.na(de_8to10$padj) &
-                       de_8to10$EnsemblID %in% TF_subset,]
-
-
-# List of TFs at 5-6ss OR 8-10ss
-NC_enriched_TFs <- unique(c(de_5to6$EnsemblID, de_8to10$EnsemblID))
-
-
-# Transcription factors which are differentially expressed in Sox8OE vs control samples and in differentially expressed in NC
-SOX8_NC_shared <- rownames(res_sub)[rownames(res_sub) %in% NC_enriched_TFs]
-
-# Plot heatmap
-NC_TF_DE_dat <- assay(rld)
-NC_TF_DE_dat <- NC_TF_DE_dat[rownames(NC_TF_DE_dat) %in% SOX8_NC_shared,]
-rownames(NC_TF_DE_dat) <- gene_annotations$gene_name[match(rownames(NC_TF_DE_dat), gene_annotations$gene_id)]
-
-
-
-
-# plot all transcription factors DE in Williams et al. 2019 using our data - keep genes which are DE in the Sox8OE
-
-png(paste0(output_path, "SOX8_NC_shared.heatmap.ens.png"),height = 10, width = 21, units = "cm", res = 200)
-pheatmap(NC_TF_DE_dat, cluster_rows=T, show_rownames=T,
-         show_colnames = F, cluster_cols=TRUE, treeheight_row = 30, treeheight_col = 30,
-         annotation_col=as.data.frame(col_data["Group"]), scale = "row",
-         main = "Shared Sox8OE enriched and NC enriched TFs \n(logFC > 1.5, padj = 0.05)",
-         border_color = NA)
-graphics.off()
-
-
-# plot all transcription factors DE in Williams et al. 2019 using our data - do not filter genes which are NOT DE in the Sox8OE
-
-NC_TF_dat <- assay(rld)
-NC_TF_dat <- NC_TF_dat[rownames(NC_TF_dat) %in% NC_enriched_TFs,]
-rownames(NC_TF_dat) <- gene_annotations$gene_name[match(rownames(NC_TF_dat), gene_annotations$gene_id)]
-
-png(paste0(output_path, "NC_enriched_TFs.heatmap.png"),height = 25, width = 21, units = "cm", res = 200)
-pheatmap(NC_TF_dat, cluster_rows=T, show_rownames=T,
-         show_colnames = F, cluster_cols=T, treeheight_row = 30, treeheight_col = 30,
-         annotation_col=as.data.frame(col_data["Group"]), scale = "row",
-         main = "NC enriched TFs - not necessarily DE between Sox8 and control \n(logFC > 1.5, padj = 0.05)",
-         border_color = NA)
-
-graphics.off()
-
-
-################################################################################
-# save CSV of norm counts and Sox8OE DEA for TFs from Williams et al. 2019
-################################################################################
-
-all_dat_NC_TF <- all_dat[all_dat$gene_id %in% NC_enriched_TFs,]
-
-cat("This table shows genes subset from Williams et al. (2019) Developmental Cell bulk RNAseq data
-Bulk RNAseq was carried out on citrine positive and citrine negative cells following electroporation of the NC specific enhancer NC1
-Sequencing was done at two stages: 5-6ss and 8-10ss
-Genes are filtered for transcription factors differentially expressed between citrine positive and negative samples (absolute FC > 1.5 and padj < 0.05) at either 5-6ss or 8-10ss
-The data presented in this table are from Sox8 overexpression and control samples
-Genes presented in this table are not necessarily differentially expressed between Sox8 overexpression and control samples \n
-Statistics:
-Normalised count: read counts adjusted for library size
-pvalue: unadjusted pvalue for differential expression test between Sox8 overexpression and control samples
-padj: pvalue for differential expression test between Sox8 overexpression and control samples - adjusted for multiple testing (Benjamini and Hochberg) \n \n",
-    file = paste0(output_path, "Supplementary_4.csv"))
-write.table(all_dat_NC_TF, paste0(output_path, "Supplementary_4.csv"), append=TRUE, row.names = F, na = 'NA', sep=",")
-
-
-
-
-
-
-
-
-
