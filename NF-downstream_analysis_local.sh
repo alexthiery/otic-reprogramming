46e05f647d185b256554649d61ff4ce70025a511
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2020-11-24 09:33:42 +0000

    add outdir to downstream run script

--- NF-downstream_analysis_local.sh
+++ NF-downstream_analysis_local.sh
@@ -3,4 +3,5 @@
 nextflow run ./NF-downstream_analysis/main.nf \
   -c ./configs/local.config \
   --input ./NF-downstream_analysis/local_samplesheet.csv \
+  --outdir output/NF-downstream_analysis \
   -resume
