20a6a53616ab49524715e96a66e514ff4bb606ff
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2020-11-30 14:00:32 +0000

    add subset plot for motifs and also plot with margins

--- NF-downstream_analysis/bin/plot_motifs.R
+++ NF-downstream_analysis/bin/plot_motifs.R
@@ -8,6 +8,7 @@ library(ggplot2)
 output_path = "./output/"
 dir.create(output_path, recursive = T)
 
+
 ######## read in data
 # read in logo data
 motif_logos = list()
@@ -23,7 +24,6 @@ motif_meta = read.delim(paste0('./ATAC_motif_enrichment/knownResults.txt'))[1:20
 motif_meta[,1] <- sub("\\(.*", "", motif_meta[,1])
 
 
-
 ####### prepare grobs
 # gene names
 gene = list(as_grob(~plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n') +
@@ -37,19 +37,33 @@ motif_names <- lapply(motif_meta[,1], function(x) {as_grob(~plot(c(0, 1), c(0, 1
 motif = list(as_grob(~plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n') +
                  text(x = 0.5, y = 0.5, "Motif", cex = 15, col = "black", font=2)))
 
-motif_logos = lapply(motif_logos, function(x) {ggseqlogo(x, method = 'prob') + theme_void()})
+motif_logos = lapply(motif_logos, function(x) {ggseqlogo(x, method = 'prob') + theme_void() + theme(plot.margin = unit(c(2,0,2,0), "cm"))})
 
 
 # pvalues
+pval = list(as_grob(~plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n') +
+                      text(x = 0.5, y = 0.5, "p-value", cex = 15, col = "black", font=2)))
+
 motif_pval <- lapply(motif_meta[,2], function(x) {as_grob(~plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n') +
                                                              text(x = 0.5, y = 0.5, x, cex = 10, col = "black"))})
 
-pval = list(as_grob(~plot(c(0, 1), c(0, 1), ann = F, bty = 'n', type = 'n', xaxt = 'n', yaxt = 'n') +
-                  text(x = 0.5, y = 0.5, "p-value", cex = 15, col = "black", font=2)))
-
-
 ######## plot grobs
-png(paste0(output_path, 'motifs.png'), width = 150, height = 200, res = 50, units = 'cm')
-grid.arrange(grobs=c(gene, motif_names, motif, motif_logos, pval, motif_pval), ncol=3, widths = c(1, 2, 1), as.table=FALSE)
+png(paste0(output_path, 'top20_motifs.png'), width = 180, height = 300, res = 100, units = 'cm')
+grid.arrange(grobs=c(gene, motif_names, motif, motif_logos, pval, motif_pval), ncol=3, widths = c(1, 3, 1), as.table=FALSE)
 graphics.off()
+
+
+
+
+########  plot selected motifs
+motifs_of_interest <- c('Sox3', 'Sox2', 'Sox10', 'TEAD3', 'Six2', 'Six1', 'Sox9', 'AP-2alpha')
+motifs_of_interest <- which(motif_meta$Motif.Name %in% motifs_of_interest)
+
+png(paste0(output_path, 'selected_motifs.png'), width = 150, height = 150, res = 100, units = 'cm')
+grid.arrange(grobs=c(gene, motif_names[motifs_of_interest], motif, motif_logos[motifs_of_interest], pval, motif_pval[motifs_of_interest]), ncol=3, widths = c(1, 4, 1), as.table=FALSE)
+graphics.off()
+
+
+
+
 
