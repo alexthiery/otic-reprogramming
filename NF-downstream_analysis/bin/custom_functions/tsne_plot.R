4827ca3a7ef481ea7350e957fa2a863eda69fe91
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2020-12-02 14:20:38 +0000

    fix tsne plot to run with ggplot

--- NF-downstream_analysis/bin/custom_functions/tsne_plot.R
+++ NF-downstream_analysis/bin/custom_functions/tsne_plot.R
@@ -1,15 +1,24 @@
 # function to plot tsne from antler object
-
-tsne_plot <- function(curr_m, gms, basename, cols=clust.colors[curr_m$cellClusters[['hclust']]$cell_ids], seed=1, pca=FALSE, perplexity=12, eta=200, plot_folder=output_path, main = NULL, ...){
+tsne_plot <- function(curr_m, gms, colour_by, colours, seed=1, pca=FALSE, perplexity=12, eta=200, main = NULL){
   set.seed(seed)
   data.logscaled =  t(scale(t(log(curr_m$getReadcounts(data_status='Normalized')[unlist(gms),]+1)), center=TRUE, scale=TRUE))
   tsne_xy = Rtsne::Rtsne(t(data.logscaled), pca=pca, perplexity=perplexity, eta=eta, max_iter=2000, verbose=T)$Y
   tsne_xy = cbind(as.data.frame(tsne_xy), cols = as.factor(m2$cellClusters$Mansel$cell_ids))
-  png(paste0(plot_folder, basename, '_TSNE.png'), ...)
-  ggplot(tsne_xy, aes(x=V1, y=V2, color=cols)) +
-    geom_point() +
-    scale_colour_manual(values = clust.colors) +
-    theme_void() +
-    theme(panel.border = element_rect(colour = "black", fill=NA, size=1), legend.position = "none")
-  dev.off()
+
+  if(length(unique(colour_by)) > length(unique(colours)) & length(unique(colours)) == 2){
+    cat('\nPlotting with continuous colour scale\n\n')
+    
+    plot_data = cbind(as.data.frame(tsne_xy), colour_by = as.numeric(colour_by))
+    plot = ggplot(plot_data, aes(x=V1, y=V2, color=colour_by)) +
+      geom_point() +
+      scale_colour_gradient(low = colours[1], high = colours[2])
+  } else {
+    cat('\nPlotting with discrete colour scale\n\n')
+    
+    plot_data = cbind(as.data.frame(tsne_xy), colour_by = as.factor(colour_by))
+    plot = ggplot(plot_data, aes(x=V1, y=V2, color=colour_by)) +
+      geom_point() +
+      scale_colour_manual(values = colours)
+  }
+  return(plot)
 }
