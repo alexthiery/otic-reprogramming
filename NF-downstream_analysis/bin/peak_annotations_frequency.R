c9c81511388fa364f457e53d2aee7a206c4006fc
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2020-12-04 15:38:55 +0000

    remove redundant peak annotation freq

--- NF-downstream_analysis/bin/peak_annotations_frequency.R
+++ NF-downstream_analysis/bin/peak_annotations_frequency.R
@@ -28,25 +28,3 @@ ggplot(freq_data, aes(x = peaks, y = Frequency)) +
         axis.text.x = element_text(angle = 45, vjust = 0.95, hjust=1)) +
   scale_y_continuous(expand = c(0, 0), limits = c(0, 0.5))
 graphics.off()
-
-
-
-# plot same as above but using detailed annotations
-detailed_annotation_peaks <- as.factor(sub(' .*', "", peaks[,"Detailed.Annotation"]))
-
-# order frequency
-freq_data <- as.data.frame(prop.table(table(detailed_annotation_peaks))[order(prop.table(table(detailed_annotation_peaks)))])
-colnames(freq_data) = c('peaks', 'Frequency')
-
-# plot frequency plot of detailed peak annotations
-png(paste0(output_path, "detailed_peak_annotation_frequency.png"), height = 10, width = 15, family = 'Arial', units = 'cm', res = 400)
-ggplot(freq_data, aes(x = peaks, y = Frequency)) +
-  geom_bar(stat='identity', fill='steelblue') +
-  theme_classic() +
-  theme(axis.title.x=element_blank(),
-        axis.ticks.x=element_blank(),
-        axis.line.x=element_blank(),
-        axis.text.x = element_text(angle = 45, vjust = 0.95, hjust=1),
-        plot.margin=unit(c(0.5,0.5,0.5,1),"cm")) +
-  scale_y_continuous(expand = c(0, 0), limits = c(0, 0.5))
-graphics.off()
