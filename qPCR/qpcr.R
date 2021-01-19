64767c128781dbd1400d30e2531674c775eaf6d8
Alex Thiery <alexandre.thiery@kcl.ac.uk>
2021-01-19 16:43:18 +0000

    finish facet wrap plot

--- qPCR/qpcr.R
+++ qPCR/qpcr.R
@@ -21,7 +21,7 @@ qPCR_data <- lapply(list.files(data_dir, pattern = '*.csv', full.names = TRUE), 
     pivot_longer(cols = which(colnames(.) != 'rowname')) %>%
     setNames(c('Sample', 'Detector', 'Cq'))
 })
-names(qPCR_data) <- sub('*.csv', '', list.files(data_dir, pattern = '*.csv'))
+  names(qPCR_data) <- sub('*.csv', '', list.files(data_dir, pattern = '*.csv'))
 
 # write re-formatted data to re-import with read.qPCR
 lapply(names(qPCR_data), function(x) {write_delim(qPCR_data[[x]], paste0(reformatted_dir, x, '.txt'))} )
@@ -30,59 +30,103 @@ lapply(names(qPCR_data), function(x) {write_delim(qPCR_data[[x]], paste0(reforma
 qPCR_data <- lapply(list.files(reformatted_dir, pattern = '*.txt', full.names = TRUE), function(x) read.qPCR(x))
 names(qPCR_data) <- sub('*.txt', '', list.files(reformatted_dir, pattern = '*.txt'))
 
-
-# calculate delta ct
-deltact <- deltaCt(qPCRBatch = qPCR_data, hkgs=c('RPLP1', 'GAPDH'), calc="geom")
-
+# calculate delta ct - if GAPDH then use both RPLP1 and GAPDH, otherwise just RPLP1
+deltact <- lapply(qPCR_data, function(x) if('GAPDH' %in% featureNames(x)){deltaCq(qPCRBatch = x, hkgs=c('RPLP1', 'GAPDH'), calc="geom")} else {deltaCq(qPCRBatch = x, hkgs='RPLP1')})
 
 
-# automatically generate comparisons matrix
-contM <- cbind(ifelse(grepl('Control', sampleNames(qPCR_data)), 1, 0), ifelse(grepl('Control', sampleNames(qPCR_data)), 0, 1))
-# add column names regardless of test sample name
-colnames(contM) <- c('Control', unique(sub('_.*', '', sampleNames(qPCR_data)))[unique(sub('_.*', '', sampleNames(qPCR_data))) != 'Control'])
-rownames(contM) <- sampleNames(qPCR_data)
 
-ddct <- deltaDeltaCq(qPCRBatch = qPCR_data, hkgs=c('GAPDH', 'RPLP1'),
-                     contrastM=contM, case=colnames(contM)[colnames(contM) != 'Control'],
-                     control="Control", hkgCalc = 'geom')
+# automatically generate comparisons matrix and add column/rownames
+contM <- list()
+for(x in names(qPCR_data)){
+  df <- cbind(ifelse(grepl('Control', sampleNames(qPCR_data[[x]])), 1, 0), ifelse(grepl('Control', sampleNames(qPCR_data[[x]])), 0, 1))
+  colnames(df) <-  c('Control', unique(sub('_.*', '', sampleNames(qPCR_data[[x]])))[unique(sub('_.*', '', sampleNames(qPCR_data[[x]]))) != 'Control'])
+  rownames(df) <- sampleNames(qPCR_data[[x]])
+  contM[[x]] <- df
+}
 
-# convert dataframe to numeric
-ddct[,2:8] <- lapply(ddct[,2:8], function(x) as.numeric(as.character(x)))
-ddct[,1] <- as.character(ddct[,1])
+
+# calculate ddct
+ddct <- lapply(names(qPCR_data), function(x) {
+  if('GAPDH' %in% featureNames(qPCR_data[[x]])){
+    deltaDeltaCq(qPCRBatch = qPCR_data[[x]], hkgs=c('RPLP1', 'GAPDH'), contrastM = contM[[x]], case=colnames(contM[[x]])[colnames(contM[[x]]) != 'Control'], control='Control', hkgCalc="geom")
+  } else {
+    deltaDeltaCq(qPCRBatch = qPCR_data[[x]], hkgs='RPLP1', contrastM = contM[[x]], case=colnames(contM[[x]])[colnames(contM[[x]]) != 'Control'], control='Control')
+    }
+  }
+)
+
+names(ddct) <- names(qPCR_data)
+
 
 # specify genes and associated colours of interest
-colors = brewer.pal(n = 5, name = "Dark2")
+library(viridis)
+
+colors = brewer.pal(n = 5, name = "Pastel1")
+
+colors = viridis(5)
 names(colors) = c('LMX1A', 'FOXG1', 'PAX2', 'SOHO1', 'ZBTB16')
 
-
-# remove genes for plotting which are not of interest
-ddct <- ddct[ddct$ID %in% names(colors),]
-
+for(x in names(ddct)){
+  
+  # convert numeric factor values to numeric, and character factor values to character
+  ddct[[x]][,2:8] <- lapply(ddct[[x]][,2:8], function(y) as.numeric(as.character(y)))
+  ddct[[x]][,1] <- as.character(ddct[[x]][,1])
+  
+  # remove genes for plotting which are not of interest
+  ddct[[x]] <- ddct[[x]][ddct[[x]][, 'ID'] %in% names(colors),]
+}
 
 
 # t-tests on delta ct
-significance <- as.data.frame(t(exprs(deltact))) %>%
-  rownames_to_column() %>%
-  mutate(rowname = sub('_.*', '', rowname)) %>%
-  gather(Gene, dct, 2:length(.)) %>%
-  group_by(Gene) %>%
-  pairwise_t_test(dct ~ rowname, p.adjust.method = 'bonferroni')
+significance <- lapply(deltact, function(x){
+  as.data.frame(t(exprs(x))) %>%
+    rownames_to_column() %>%
+    mutate(rowname = sub('_.*', '', rowname)) %>%
+    gather(Gene, dct, 2:length(.)) %>%
+    group_by(Gene) %>%
+    pairwise_t_test(dct ~ rowname, p.adjust.method = 'bonferroni')
+})
 
 
+plot_data <- list()
 # add significance to ddct dataframe
-ddct$sig <- significance %>%
-  filter(Gene %in% ddct$ID) %>%
-  arrange(ddct$ID) %>%
-  pull(p.adj.signif)
+for(x in names(ddct)){
+  plot_data[[x]] <- ddct[[x]][,c(1, 6:8)]
+  plot_data[[x]] <- cbind(plot_data[[x]], sample = x)
+  plot_data[[x]][, 'sig'] <- significance[[x]] %>%
+    filter(Gene %in% ddct[[x]][, 'ID']) %>%
+    arrange(ddct[[x]][, 'ID']) %>%
+    pull(p.adj.signif)
+}
+
+# combine all experiments into a single data frame for plotting
+plot_data <- reduce(plot_data, rbind)
+
+# change experiment order for facet wrap order
+
+plot_data$sample <- sub('_MO', ' MO', plot_data$sample)
+
+plot_data$sample <- factor(plot_data$sample, levels = c('SOX8 MO', 'PAX2 MO', 'LMX1A MO', 'ZBTB16 MO'))
 
 
 
-ggplot(ddct, aes(y = `2^-ddCt`, x = ID, fill = ID, label = ifelse(sig != 'ns', sig, ''))) +
+ggplot(plot_data, aes(y = `2^-ddCt`, x = ID, fill = ID, label = ifelse(sig != 'ns', sig, ''))) +
   geom_bar(stat='identity') +
   geom_text(aes(y = `2^-ddCt.max` + 0.1), size = 10) +
   geom_errorbar(aes(ymin = `2^-ddCt.min`, ymax = `2^-ddCt.max`), width = 0.5) +
-  scale_fill_manual(values = colors[ddct$ID]) +
-  theme_classic()
+  scale_fill_manual(values = colors[plot_data$ID]) +
+  scale_y_continuous(expand = c(0, 0)) +
+  theme_classic() +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        legend.title = element_blank()) +
+  facet_wrap(~sample, ncol = 4, strip.position = "bottom") +
+  theme(strip.background = element_blank(),
+        strip.text = element_text(size=10),
+        strip.placement = "outside") +
+  ylab(bquote(paste('fold change (', 2^-~Delta*Delta*Ct, ' )')))
+
 
 
 
